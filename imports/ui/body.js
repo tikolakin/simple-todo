12bb0289fee666502a8b2dace6f0b68bcaca86c9
Tikolakin <tikolakin@gmail.com>
2016-06-07 01:06:53 +0300

    Storing temporary UI state in a Reactive Dictionary

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,13 +1,29 @@
 import { Template } from 'meteor/templating';
 
+import { ReactiveDict } from 'meteor/reactive-dict';
+
 import { Tasks } from '../api/tasks.js';
 
 import './task.js';
 import './body.html';
 
+Template.body.onCreated(function bodyOnCreate() {
+    this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
     tasks() {
+        const instance = Template.instance();
+        if (!instance.state.get('hideCompleted')) {
+            // If hide complated is checked - filter tasks
+            return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+        }
+
+        // Otherwise reutrn all of the tasks
         return Tasks.find({}, { sort: { createdAt: -1 } });
+    },
+    incomplatedCount() {
+        return Tasks.find({ checked: { $ne: true}}).count();
     }
 });
 
@@ -28,5 +44,11 @@ Template.body.events({
 
         // Clear form
         target.text.value = '';
-    }
+    },
+    'change .hide-completed input'(event, instance) {
+        instance.state.set('hideCompleted', event.target.checked);
+    },
+    'change .toggle-checked input'(event, instance) {
+        instance.state.set('hideCompleted', event.target.checked);
+    },
 });
